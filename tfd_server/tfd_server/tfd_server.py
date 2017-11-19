5e0d226291c8730bee4d15adf4e9479fee8a75e6
erikiado <erikiado@gmail.com>
2017-11-19 14:54:46 -0600

    change for public

--- tfd_server/tfd_server/tfd_server.py
+++ tfd_server/tfd_server/tfd_server.py
@@ -4,7 +4,7 @@ import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
 
-app = Flask(__name__) # create the application instance :)
+app = Flask(__name__, static_folder="/public") # create the application instance :)
 app.config.from_object(__name__) # load config from this file , flaskr.py
 
 # Load default config and override config from an environment variable
